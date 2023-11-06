5914dd1539e9661e4c63e21e368877ed62e66b27
Deeptesh chaudhari <diptesh151097@gmail.com>
2023-11-06 15:34:23 +0530

    .

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,14 +2,13 @@ const mongoose = require("mongoose");
 
 mongoose.set("strictQuery", false);
 
-mongoose.connect("mongodb+srv://diptesh151097:Mahadev123@cluster0.dgyjmsj.mongodb.net/", {
+mongoose.connect(process.env.REACT_APP_MONGODB_URI || "mongodb+srv://diptesh151097:Mahadev123@cluster0.dgyjmsj.mongodb.net/", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }, err => {
   if (err) throw err;
   console.log('Connected to MongoDB!')
 }
-
 );
 
 module.exports = mongoose.connection;
--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const routes = require("./routes");
 const db = require("./config/connection");
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
@@ -11,11 +11,11 @@ app.use(express.json());
 
 // Serve up static assets
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use(express.static(path.join(__dirname, 'client', 'build')));
 }
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
 
 app.use(routes);
